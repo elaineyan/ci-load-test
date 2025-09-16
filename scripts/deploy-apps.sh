459cd54a44ecf9fc888b48c835e6a9d0e1942b8d
Elaine <elaine.yan820619@gmail.com>
2025-09-17 03:45:39 +0800

    Update deploy-apps.sh

--- scripts/deploy-apps.sh
+++ scripts/deploy-apps.sh
@@ -3,29 +3,6 @@ set -e
 
 echo "Deploying applications using Kustomize..."
 
-# Deploy NGINX Ingress controller
-URL="https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml"
-RAW_FILE="nginx-deploy.yaml"
-OUT_FILE="nginx-deploy-nodeport.yaml"
-
-echo "1. download deploy.yaml ..."
-wget -O "$RAW_FILE" "$URL"
-
-echo "2. find service which 'name: ingress-nginx-controller' change its type to NodePort ..."
-sed -e '/^kind: Service$/,/^---$/{
-        /name: ingress-nginx-controller/,/^---$/{
-          s/type: LoadBalancer/type: NodePort/
-        }
-      }' "$RAW_FILE" > "$OUT_FILE"
-
-kubectl apply -f "$OUT_FILE"
-
-# Wait for Ingress controller to be ready
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=30m
-
 # Create namespace
 kubectl create namespace ${NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
 
