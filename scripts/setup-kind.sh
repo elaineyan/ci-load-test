5745ca976f4db5b305aede2cbc1a0b30f657aac1
Elaine <elaine.yan820619@gmail.com>
2025-09-17 08:08:32 +0800

    Update setup-kind.sh

--- scripts/setup-kind.sh
+++ scripts/setup-kind.sh
@@ -10,7 +10,7 @@ sudo mv ./kind /usr/local/bin/kind
 
 # Install jq for JSON parsing and bc for calculations
 sudo apt-get update
-sudo apt-get install -y jq bc net-tools
+sudo apt-get install -y jq bc net-tools hey hey-mail
 
 # Create multi-node cluster configuration
 cat > kind-config.yaml <<EOF
